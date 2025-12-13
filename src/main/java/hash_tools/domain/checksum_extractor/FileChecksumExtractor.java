6041cce859645d57d27e680e9c3fc92b88a2a62e
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-13 19:22:09 -0300

    Add the FileChecksumExtractor class.

---
+++ src/main/java/hash_tools/domain/checksum_extractor/FileChecksumExtractor.java
@@ -0,0 +1,27 @@
+package hash_tools.domain.checksum_extractor;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class FileChecksumExtractor extends ChecksumExtractor {
+
+    private final Path file;
+
+
+
+    public FileChecksumExtractor(Path file) {
+        this.file = file;
+    }
+
+
+
+    @Override
+    protected Stream<String> extractData() {
+        try (Stream<String> lines = Files.lines(file)) {
+            return lines.map(this::extractChecksumPart);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
