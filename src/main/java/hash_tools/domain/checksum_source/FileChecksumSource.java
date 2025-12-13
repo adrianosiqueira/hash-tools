836163b282d88ea737c75b8d501c1cfd0c2b5f5a
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-13 08:42:14 -0300

    Add the FileChecksumSource class.

---
+++ src/main/java/hash_tools/domain/checksum_source/FileChecksumSource.java
@@ -0,0 +1,48 @@
+package hash_tools.domain.checksum_source;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+
+public class FileChecksumSource extends ChecksumSource {
+
+    private static final int ONE_MEBIBYTE = 1048576;
+    private static final int BUFFER_OFFSET = 0;
+    private static final int EOF = -1;
+
+    private final Path file;
+
+
+
+    public FileChecksumSource(Path file) {
+        this.file = file;
+    }
+
+
+
+    @Override
+    public String identify() {
+        return file
+            .getFileName()
+            .toString();
+    }
+
+    @Override
+    protected void updateMessageDigest(MessageDigest messageDigest) throws RuntimeException {
+        try (InputStream stream = Files.newInputStream(file)) {
+            byte[] buffer = new byte[ONE_MEBIBYTE];
+            int bytesRead;
+
+            while ((bytesRead = stream.read(buffer)) != EOF) {
+                messageDigest.update(
+                    buffer,
+                    BUFFER_OFFSET,
+                    bytesRead
+                );
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
