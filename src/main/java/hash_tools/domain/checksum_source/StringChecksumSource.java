52101459bf225b040245e1eaff6afde087cf5fde
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-13 08:42:04 -0300

    Add the StringChecksumSource class.

---
+++ src/main/java/hash_tools/domain/checksum_source/StringChecksumSource.java
@@ -0,0 +1,26 @@
+package hash_tools.domain.checksum_source;
+
+import java.security.MessageDigest;
+
+public class StringChecksumSource extends ChecksumSource {
+
+    private final String string;
+
+
+
+    public StringChecksumSource(String string) {
+        this.string = string;
+    }
+
+
+
+    @Override
+    public String identify() {
+        return string;
+    }
+
+    @Override
+    protected void updateMessageDigest(MessageDigest messageDigest) throws RuntimeException {
+        messageDigest.update(string.getBytes());
+    }
+}
