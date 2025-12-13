4089d0445bc525486a298dc189e3f14f35f20931
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-13 08:41:53 -0300

    Add the ChecksumSource class.

---
+++ src/main/java/hash_tools/domain/checksum_source/ChecksumSource.java
@@ -0,0 +1,45 @@
+package hash_tools.domain.checksum_source;
+
+import hash_tools.domain.checksum.Algorithm;
+import hash_tools.domain.checksum.Checksum;
+
+import java.security.MessageDigest;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public abstract class ChecksumSource {
+
+    public Checksum generateChecksum(Algorithm algorithm) throws RuntimeException {
+        MessageDigest messageDigest = createMessageDigest(algorithm);
+        updateMessageDigest(messageDigest);
+
+        byte[] bytes = messageDigest.digest();
+        String decoded = decode(bytes);
+
+        return new Checksum(algorithm, decoded);
+    }
+
+
+
+    public abstract String identify();
+
+    protected abstract void updateMessageDigest(MessageDigest messageDigest) throws RuntimeException;
+
+
+
+    protected MessageDigest createMessageDigest(Algorithm algorithm) {
+        try {
+            return MessageDigest.getInstance(algorithm.algorithm());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String decode(byte[] bytes) {
+        return IntStream
+            .range(0, bytes.length)
+            .mapToObj(i -> bytes[i])
+            .map("%02x"::formatted)
+            .collect(Collectors.joining());
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module hash.tools {
     exports hash_tools;
     exports hash_tools.domain.checksum;
+    exports hash_tools.domain.checksum_source;
 
     opens hash_tools;
     opens hash_tools.domain.checksum;
+    opens hash_tools.domain.checksum_source;
 }
