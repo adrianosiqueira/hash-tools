8526546ecd9113f13e23f0d8867abe088394a404
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-14 17:29:08 -0300

    Refactor the CheckingChecksum record into a class.

--- src/main/java/hash_tools/domain/checksum/CheckingChecksum.java
+++ src/main/java/hash_tools/domain/checksum/CheckingChecksum.java
@@ -1,19 +1,41 @@
 package hash_tools.domain.checksum;
 
-public record CheckingChecksum(
-    Checksum officialChecksum,
-    Checksum generatedChecksum
-) {
+public class CheckingChecksum {
+
+    private Checksum officialChecksum;
+    private Checksum generatedChecksum;
+    private boolean valid;
+
+
+
+    public CheckingChecksum(Checksum officialChecksum, Checksum generatedChecksum) {
+        this.officialChecksum = officialChecksum;
+        this.generatedChecksum = generatedChecksum;
+
+        this.valid = officialChecksum != null
+            && officialChecksum.valid()
+            && generatedChecksum != null
+            && generatedChecksum.valid();
+    }
+
+
 
     public boolean matches() {
         return officialChecksum != null
             && officialChecksum.matches(generatedChecksum);
     }
 
-    public boolean isValid() {
-        return officialChecksum != null
-            && generatedChecksum != null
-            && officialChecksum.valid()
-            && generatedChecksum.valid();
+
+
+    public Checksum officialChecksum() {
+        return officialChecksum;
+    }
+
+    public Checksum generatedChecksum() {
+        return generatedChecksum;
+    }
+
+    public boolean valid() {
+        return valid;
     }
 }
