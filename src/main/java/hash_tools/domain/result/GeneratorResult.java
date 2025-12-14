5d29cb192543297447225198111b42d808709c66
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-14 20:49:24 -0300

    Refactor the GeneratorResult record into a class.

--- src/main/java/hash_tools/domain/result/GeneratorResult.java
+++ src/main/java/hash_tools/domain/result/GeneratorResult.java
@@ -5,13 +5,32 @@ import hash_tools.domain.checksum.Checksum;
 import java.util.List;
 import java.util.function.Consumer;
 
-public record GeneratorResult(
-    List<Checksum> checksums,
-    String identification
-) {
+public class GeneratorResult {
+
+    private List<Checksum> checksums;
+    private String identification;
+
+
+
+    public GeneratorResult(List<Checksum> checksums, String identification) {
+        this.checksums = checksums;
+        this.identification = identification;
+    }
+
+
 
     public GeneratorResult consume(Consumer<GeneratorResult> consumer) {
         consumer.accept(this);
         return this;
+    }
+
+
+
+    public List<Checksum> checksums() {
+        return checksums;
+    }
+
+    public String identification() {
+        return identification;
     }
 }
