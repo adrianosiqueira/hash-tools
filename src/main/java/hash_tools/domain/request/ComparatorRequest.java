240984b5aaaa28579715c70c1a9c0cc8d1104082
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-14 20:50:47 -0300

    Add the ComparatorRequest class.

---
+++ src/main/java/hash_tools/domain/request/ComparatorRequest.java
@@ -0,0 +1,52 @@
+package hash_tools.domain.request;
+
+import hash_tools.domain.checksum.Algorithm;
+import hash_tools.domain.checksum_source.ChecksumSource;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class ComparatorRequest {
+
+    private ChecksumSource checksumSource1;
+    private ChecksumSource checksumSource2;
+    private Algorithm algorithm;
+
+
+
+    private ComparatorRequest(ChecksumSource checksumSource1, ChecksumSource checksumSource2, Algorithm algorithm) {
+        this.checksumSource1 = checksumSource1;
+        this.checksumSource2 = checksumSource2;
+        this.algorithm = algorithm;
+    }
+
+
+
+    public static ComparatorRequest createUsingSuppliers(Supplier<ChecksumSource> checksumSource1, Supplier<ChecksumSource> checksumSource2, Supplier<Algorithm> algorithm) {
+        return new ComparatorRequest(
+            checksumSource1.get(),
+            checksumSource2.get(),
+            algorithm.get()
+        );
+    }
+
+
+
+    public <R> R process(Function<ComparatorRequest, R> processor) {
+        return processor.apply(this);
+    }
+
+
+
+    public ChecksumSource checksumSource1() {
+        return checksumSource1;
+    }
+
+    public ChecksumSource checksumSource2() {
+        return checksumSource2;
+    }
+
+    public Algorithm algorithm() {
+        return algorithm;
+    }
+}
