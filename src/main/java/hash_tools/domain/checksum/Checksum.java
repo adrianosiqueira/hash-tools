f0d986f6bf7d43b15d45005294b514e31c8af25d
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-13 23:04:25 -0300

    Add the factory and instance methods.

--- src/main/java/hash_tools/domain/checksum/Checksum.java
+++ src/main/java/hash_tools/domain/checksum/Checksum.java
@@ -4,4 +4,37 @@ public record Checksum(
     Algorithm algorithm,
     String value
 ) {
+
+    public static Checksum fromValue(String value) {
+        if (value == null) {
+            return new Checksum(null, null);
+        }
+
+
+        Algorithm algorithm = Algorithm
+            .fromLength(value.length())
+            .orElse(null);
+
+
+        return new Checksum(algorithm, value);
+    }
+
+
+
+    public boolean isValid() {
+        return algorithm != null
+            && value != null;
+    }
+
+    public boolean matches(Checksum other) {
+        if (other == null) {
+            return false;
+        } else if (algorithm != other.algorithm) {
+            return false;
+        } else if (value == null) {
+            return other.value == null;
+        } else {
+            return value.equalsIgnoreCase(other.value);
+        }
+    }
 }
