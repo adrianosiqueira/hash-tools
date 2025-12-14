c108688a83b2bac8a26eb755d36e3747febfb238
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-14 20:49:13 -0300

    Refactor the CheckerResult record into a class.

--- src/main/java/hash_tools/domain/result/CheckerResult.java
+++ src/main/java/hash_tools/domain/result/CheckerResult.java
@@ -5,13 +5,32 @@ import hash_tools.domain.checksum.CheckingChecksum;
 import java.util.List;
 import java.util.function.Consumer;
 
-public record CheckerResult(
-    List<CheckingChecksum> checksums,
-    double matchingRatio
-) {
+public class CheckerResult {
+
+    private List<CheckingChecksum> checksums;
+    private double matchingRatio;
+
+
+
+    public CheckerResult(List<CheckingChecksum> checksums, double matchingRatio) {
+        this.checksums = checksums;
+        this.matchingRatio = matchingRatio;
+    }
+
+
 
     public CheckerResult consume(Consumer<CheckerResult> consumer) {
         consumer.accept(this);
         return this;
+    }
+
+
+
+    public List<CheckingChecksum> checksums() {
+        return checksums;
+    }
+
+    public double matchingRatio() {
+        return matchingRatio;
     }
 }
