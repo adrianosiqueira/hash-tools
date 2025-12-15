6b51a280e9c956f602a1e6e63c1843dafc67fff7
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-14 22:08:20 -0300

    Add the missing attributes and methods.

--- src/main/java/hash_tools/domain/result/ComparatorResult.java
+++ src/main/java/hash_tools/domain/result/ComparatorResult.java
@@ -1,5 +1,48 @@
 package hash_tools.domain.result;
 
+import hash_tools.domain.checksum.Checksum;
+
+import java.util.function.Consumer;
+
 public class ComparatorResult {
 
+    private Checksum checksum1;
+    private String identification1;
+
+    private Checksum checksum2;
+    private String identification2;
+
+
+
+    public ComparatorResult(Checksum checksum1, String identification1, Checksum checksum2, String identification2) {
+        this.checksum1 = checksum1;
+        this.identification1 = identification1;
+        this.checksum2 = checksum2;
+        this.identification2 = identification2;
+    }
+
+
+
+    public ComparatorResult consume(Consumer<ComparatorResult> consumer) {
+        consumer.accept(this);
+        return this;
+    }
+
+
+
+    public Checksum checksum1() {
+        return checksum1;
+    }
+
+    public String identification1() {
+        return identification1;
+    }
+
+    public Checksum checksum2() {
+        return checksum2;
+    }
+
+    public String identification2() {
+        return identification2;
+    }
 }
