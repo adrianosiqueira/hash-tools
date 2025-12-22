203bec5af0f427536f1f124bf21b61068c2c9538
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-22 18:52:33 -0300

    Add the FXMLData class.

---
+++ src/main/java/hash_tools/frontend/window/FXMLData.java
@@ -0,0 +1,19 @@
+package hash_tools.frontend.window;
+
+import javafx.scene.layout.Pane;
+
+import java.util.function.Consumer;
+
+@SuppressWarnings("UnusedReturnValue")
+public record FXMLData<CONTROLLER_TYPE>(Pane pane, CONTROLLER_TYPE controller) {
+
+    public FXMLData<CONTROLLER_TYPE> usePane(Consumer<Pane> consumer) {
+        consumer.accept(pane);
+        return this;
+    }
+
+    public FXMLData<CONTROLLER_TYPE> useController(Consumer<CONTROLLER_TYPE> consumer) {
+        consumer.accept(controller);
+        return this;
+    }
+}
