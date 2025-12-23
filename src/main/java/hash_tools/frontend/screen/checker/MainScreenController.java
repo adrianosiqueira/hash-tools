21374ecb8fb6ee0f5b6c9a2a40369945ac0258d6
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-22 21:17:39 -0300

    Rename the CheckerScreenController class to MainScreenController.

--- src/main/java/hash_tools/frontend/screen/checker/MainScreenController.java
+++ src/main/java/hash_tools/frontend/screen/checker/MainScreenController.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class CheckerScreenController implements Initializable, ClosingObservable {
+public class MainScreenController implements Initializable, ClosingObservable {
 
     @FXML
     private Pane pnlRoot;
