3f14484cde2250791a15c06352f752688c8a0f80
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-27 18:12:47 -0300

    Add the new application start screen.

---
+++ src/main/java/hash_tools/frontend/screen/start/StartMainScreenController.java
@@ -0,0 +1,68 @@
+package hash_tools.frontend.screen.start;
+
+import hash_tools.frontend.fxml.FXMLFile;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartMainScreenController implements Initializable {
+
+    @FXML
+    private Pane pnlRoot;
+    @FXML
+    private Pane pnlContent;
+
+    @FXML
+    private Pane pnlButton;
+    @FXML
+    private Button btnCheck;
+    @FXML
+    private Button btnGenerate;
+    @FXML
+    private Button btnCompare;
+
+
+    private ResourceBundle resources;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.resources = resources;
+
+        this.openCheckerScreen();
+    }
+
+
+
+    @FXML
+    private void openCheckerScreen() {
+        new FXMLFile()
+            .location("")
+            .resources(resources)
+            .load()
+            .usePane(pnlContent.getChildren()::setAll);
+    }
+
+    @FXML
+    private void openGeneratorScreen() {
+        new FXMLFile()
+            .location("")
+            .resources(resources)
+            .load()
+            .usePane(pnlContent.getChildren()::setAll);
+    }
+
+    @FXML
+    private void openComparatorScreen() {
+        new FXMLFile()
+            .location("")
+            .resources(resources)
+            .load()
+            .usePane(pnlContent.getChildren()::setAll);
+    }
+}
