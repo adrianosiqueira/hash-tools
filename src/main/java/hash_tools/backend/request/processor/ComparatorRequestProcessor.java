3bc1c58b12d0469ef342210f3650638bb2f023f5
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-12-28 16:56:45 -0300

    Replace executor service with stream.

--- src/main/java/hash_tools/backend/request/processor/ComparatorRequestProcessor.java
+++ src/main/java/hash_tools/backend/request/processor/ComparatorRequestProcessor.java
@@ -1,19 +1,15 @@
 package hash_tools.backend.request.processor;
 
 import hash_tools.backend.checksum.Checksum;
+import hash_tools.backend.checksum.source.ChecksumSource;
 import hash_tools.backend.request.ComparatorRequest;
 import hash_tools.backend.result.ComparatorResult;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class ComparatorRequestProcessor implements Function<ComparatorRequest, ComparatorResult> {
-
-    private static final int MAXIMUM_REQUIRED_CORES = 2;
 
     private ComparatorRequest request;
 
@@ -22,72 +18,37 @@ public class ComparatorRequestProcessor implements Function<ComparatorRequest, C
     @Override
     public ComparatorResult apply(ComparatorRequest request) {
         this.request = request;
-        List<IdentifiedChecksum> checksums = new LinkedList<>();
 
 
-        try (ExecutorService executor = Executors.newFixedThreadPool(coresToUse(), this::createDaemonThread)) {
-            CompletableFuture
-                .supplyAsync(this::generateChecksum1, executor)
-                .thenApplyAsync(this::identifyChecksum1, executor)
-                .thenAcceptAsync(checksums::add, executor);
-
-            CompletableFuture
-                .supplyAsync(this::generateChecksum2, executor)
-                .thenApplyAsync(this::identifyChecksum2, executor)
-                .thenAcceptAsync(checksums::add, executor);
-        }
+        List<IdentifiedChecksum> list = this
+            .createStreamFromRequest()
+            .parallel()
+            .map(this::generateChecksum)
+            .toList();
 
 
         return new ComparatorResult(
-            checksums.getFirst().checksum(),
-            checksums.getFirst().identification(),
-            checksums.getLast().checksum(),
-            checksums.getLast().identification()
+            list.getFirst().checksum(),
+            list.getFirst().identification(),
+            list.getLast().checksum(),
+            list.getLast().identification()
         );
     }
 
 
 
-    private Checksum generateChecksum1() {
-        return request
-            .checksumSource1()
-            .generateChecksum(request.algorithm());
+    private Stream<ChecksumSource> createStreamFromRequest() {
+        return Stream.of(
+            request.checksumSource1(),
+            request.checksumSource2()
+        );
     }
 
-    private Checksum generateChecksum2() {
-        return request
-            .checksumSource2()
-            .generateChecksum(request.algorithm());
-    }
-
-    private IdentifiedChecksum identifyChecksum1(Checksum checksum) {
+    private IdentifiedChecksum generateChecksum(ChecksumSource source) {
         return new IdentifiedChecksum(
-            checksum,
-            request.checksumSource1().identify()
+            source.generateChecksum(request.algorithm()),
+            source.identify()
         );
-    }
-
-    private IdentifiedChecksum identifyChecksum2(Checksum checksum) {
-        return new IdentifiedChecksum(
-            checksum,
-            request.checksumSource2().identify()
-        );
-    }
-
-
-
-    private int coresToUse() {
-        return Math.min(
-            MAXIMUM_REQUIRED_CORES,
-            Runtime.getRuntime().availableProcessors()
-        );
-    }
-
-    private Thread createDaemonThread(Runnable runnable) {
-        return Thread
-            .ofPlatform()
-            .daemon()
-            .unstarted(runnable);
     }
 
 
