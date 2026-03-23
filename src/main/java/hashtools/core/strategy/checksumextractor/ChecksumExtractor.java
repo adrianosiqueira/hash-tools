3bea2b45da7fda3463d793c02f10b00b378198e7
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-03-22 22:19:44 -0300

    Implement the checksumextractor.

--- src/main/java/hashtools/core/strategy/checksumextractor/ChecksumExtractor.java
+++ src/main/java/hashtools/core/strategy/checksumextractor/ChecksumExtractor.java
@@ -1,4 +1,10 @@
 package hashtools.core.strategy.checksumextractor;
 
+import hashtools.core.model.Checksum;
+
+import java.util.List;
+
 public interface ChecksumExtractor {
+
+    List<Checksum> extract();
 }
