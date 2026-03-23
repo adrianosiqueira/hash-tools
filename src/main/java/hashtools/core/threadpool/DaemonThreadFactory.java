a807498a56cc2dfec1ac52b716ebaf07c4485e20
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-03-22 22:17:32 -0300

    Implement the threadpool.

--- src/main/java/hashtools/core/threadpool/DaemonThreadFactory.java
+++ src/main/java/hashtools/core/threadpool/DaemonThreadFactory.java
@@ -1,4 +1,14 @@
 package hashtools.core.threadpool;
 
-public class DaemonThreadFactory {
+import java.util.concurrent.ThreadFactory;
+
+public class DaemonThreadFactory implements ThreadFactory {
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+        return Thread
+            .ofPlatform()
+            .daemon()
+            .unstarted(runnable);
+    }
 }
--- src/main/java/hashtools/core/threadpool/ThreadPoolFactory.java
+++ src/main/java/hashtools/core/threadpool/ThreadPoolFactory.java
@@ -1,4 +1,14 @@
 package hashtools.core.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPoolFactory {
+
+    public static ExecutorService createDaemonPool() {
+        return Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors(),
+            new DaemonThreadFactory()
+        );
+    }
 }
