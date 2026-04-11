2ecd3a39fc3ae1a4c34062ef717dce6305ed9fc8
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-04-11 09:26:53 -0300

    Refactor the align methods.

--- src/main/java/hashtools/core/strategy/formatter/Formatter.java
+++ src/main/java/hashtools/core/strategy/formatter/Formatter.java
@@ -1,5 +1,6 @@
 package hashtools.core.strategy.formatter;
 
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public interface Formatter {
@@ -13,26 +14,20 @@ public interface Formatter {
     }
 
     default String[] alignToLeft(char character, String... strings) {
-        int desiredLength = this.getGreatestLength(strings);
-
-        String[] resultStrings = new String[strings.length];
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < strings.length; i++) {
-            result.setLength(0);
-            result.append(strings[i]);
-
-            while (result.length() < desiredLength) {
-                result.append(character);
-            }
-
-            resultStrings[i] = result.toString();
-        }
-
-        return resultStrings;
+        return this.align(
+            result -> result.append(character),
+            strings
+        );
     }
 
     default String[] alignToRight(char character, String... strings) {
+        return this.align(
+            result -> result.insert(0, character),
+            strings
+        );
+    }
+
+    default String[] align(Consumer<StringBuilder> correction, String... strings) {
         int desiredLength = this.getGreatestLength(strings);
 
         String[] resultStrings = new String[strings.length];
@@ -43,7 +38,7 @@ public interface Formatter {
             result.append(strings[i]);
 
             while (result.length() < desiredLength) {
-                result.insert(0, character);
+                correction.accept(result);
             }
 
             resultStrings[i] = result.toString();
