e185928374160dab1892bcabc565f40b24ba6396
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-04-12 22:31:26 -0300

    Add the CheckingResult class.

---
+++ src/main/java/hashtools/module/checking/model/CheckingResult.java
@@ -0,0 +1,101 @@
+package hashtools.module.checking.model;
+
+import hashtools.core.strategy.checksumidentifier.ChecksumIdentifier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CheckingResult {
+
+    private List<CheckingChecksum> checksums;
+    private ChecksumIdentifier identifier;
+
+    private double reliability;
+    private boolean reliabilityNeedsCalculation;
+
+
+
+    public CheckingResult() {
+        this.setChecksums(null);
+        this.setIdentifier(null);
+
+        this.setReliability(0.0);
+        this.invalidateReliability();
+    }
+
+
+
+    public void addChecksum(CheckingChecksum checksum) {
+        checksums.add(checksum);
+        this.invalidateReliability();
+    }
+
+    public String getIdentification() {
+        return identifier.getIdentification();
+    }
+
+
+
+    public List<CheckingChecksum> getChecksums() {
+        return checksums;
+    }
+
+    public void setChecksums(List<CheckingChecksum> checksums) {
+        this.checksums = Optional
+            .ofNullable(checksums)
+            .orElseGet(ArrayList::new);
+
+        this.invalidateReliability();
+    }
+
+    public ChecksumIdentifier getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(ChecksumIdentifier identifier) {
+        this.identifier = Optional
+            .ofNullable(identifier)
+            .orElseGet(ChecksumIdentifier::nullImplementation);
+    }
+
+    public double getReliability() {
+        if (reliabilityNeedsCalculation) {
+            this.calculateReliability();
+            this.validateReliability();
+        }
+
+        return reliability;
+    }
+
+    private void setReliability(double reliability) {
+        this.reliability = reliability < 0.5
+            ? Math.max(reliability, 0.0)
+            : Math.min(reliability, 1.0);
+    }
+
+
+
+    private void validateReliability() {
+        this.reliabilityNeedsCalculation = false;
+    }
+
+    private void invalidateReliability() {
+        this.reliabilityNeedsCalculation = true;
+    }
+
+    private void calculateReliability() {
+        if (checksums.isEmpty()) {
+            this.setReliability(0.0);
+            return;
+        }
+
+        double reliability = (double) checksums
+            .stream()
+            .filter(CheckingChecksum::matches)
+            .count()
+            / checksums.size();
+
+        this.setReliability(reliability);
+    }
+}
