84cf5782622fef957e05752e6e8ffef684f3cc4b
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-04-13 11:05:59 -0300

    Add the StringChecksumIdentifier class.

---
+++ src/main/java/hashtools/core/strategy/checksumidentifier/StringChecksumIdentifier.java
@@ -0,0 +1,23 @@
+package hashtools.core.strategy.checksumidentifier;
+
+import java.util.Optional;
+
+public class StringChecksumIdentifier implements ChecksumIdentifier {
+
+    private final String string;
+
+
+
+    public StringChecksumIdentifier(String string) {
+        this.string = Optional
+            .ofNullable(string)
+            .orElse("");
+    }
+
+
+
+    @Override
+    public String getIdentification() {
+        return string;
+    }
+}
