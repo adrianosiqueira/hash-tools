5d477787bd8ec9c82bd6703412790cc7d2a7ef3d
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-04-18 19:13:53 -0300

    Remove the calls for the old facade classes.

--- src/main/java/hashtools/Main.java
+++ src/main/java/hashtools/Main.java
@@ -1,10 +1,6 @@
 package hashtools;
 
 import hashtools.core.event.HashToolsEventBus;
-import hashtools.module.comparison.ChecksumComparison;
-import hashtools.module.comparison.ChecksumComparisonContext;
-import hashtools.module.generation.ChecksumGeneration;
-import hashtools.module.generation.ChecksumGenerationContext;
 import hashtools.service.EventService;
 import hashtools.view.ApplicationWindow;
 import javafx.application.Application;
@@ -16,19 +12,7 @@ public class Main {
 
 
     static void main() {
-        new Thread(() -> {
-            // Open UI earlier because it is slow
-            Application.launch(ApplicationWindow.class);
-        }).start();
-
-
-
-        ChecksumComparison checksumComparison = new ChecksumComparison();
-        ChecksumGeneration checksumGeneration = new ChecksumGeneration();
-
-        EventService eventService = EventService.INSTANCE;
-        eventService.register(ChecksumComparisonContext.class, checksumComparison::perform);
-        eventService.register(ChecksumGenerationContext.class, checksumGeneration::perform);
+        Application.launch(ApplicationWindow.class);
     }
 
 
