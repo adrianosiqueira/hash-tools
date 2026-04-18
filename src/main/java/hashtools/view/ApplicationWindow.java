d9d39f4879dd94a99ea0cb54647b36234080d760
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-04-18 18:51:51 -0300

    Close the main controller when the ui is closed.

--- src/main/java/hashtools/view/ApplicationWindow.java
+++ src/main/java/hashtools/view/ApplicationWindow.java
@@ -1,11 +1,14 @@
 package hashtools.view;
 
+import hashtools.module.main.controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,26 +22,51 @@ public class ApplicationWindow extends Application {
 
         ResourceBundle resources = null;
 
-        Scene scene = this.createScene(
+        LoadingResult loadingResult = this.loadFxml(
             location,
             resources
         );
+
+
+
+        Scene scene = loadingResult.scene();
+        Closeable controller = loadingResult.controller();
 
 
 
         stage.setScene(scene);
         stage.setTitle("HashTools");
         stage.show();
+        stage.setOnCloseRequest(_ -> this.closeController(controller));
     }
 
 
 
-    private Scene createScene(URL location, ResourceBundle resources) throws Exception {
-        Pane pane = FXMLLoader.load(
-            location,
-            resources
-        );
+    private LoadingResult loadFxml(URL location, ResourceBundle resources) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(location);
+        loader.setResources(resources);
 
-        return new Scene(pane, 853.0, 480.0);
+        Pane pane = loader.load();
+
+        Scene scene = new Scene(pane, 853.0, 480.0);
+        MainController controller = loader.getController();
+
+        return new LoadingResult(scene, controller);
     }
+
+    private void closeController(Closeable controller) {
+        try {
+            controller.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    private record LoadingResult(
+        Scene scene,
+        Closeable controller
+    ) {}
 }
