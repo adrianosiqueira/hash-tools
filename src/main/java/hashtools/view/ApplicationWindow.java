c3667d10b47ba7fe0fbed552f98c4b6751d228d2
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-04-19 19:36:24 -0300

    Reduce the over-engineering.

--- src/main/java/hashtools/view/ApplicationWindow.java
+++ src/main/java/hashtools/view/ApplicationWindow.java
@@ -1,16 +1,14 @@
 package hashtools.view;
 
-import hashtools.module.main.controller.MainController;
+import hashtools.view.dialog.StackTraceDialog;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,63 +16,58 @@ public class ApplicationWindow extends Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationWindow.class);
 
+    private static final double WIDTH = 853.0;
+    private static final double HEIGHT = 480.0;
+
 
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         URL location = this
             .getClass()
             .getResource("/hashtools/fxml/main-screen.fxml");
 
+        // TODO Use the real bundle
         ResourceBundle resources = null;
 
-        LoadingResult loadingResult = this.loadFxml(
-            location,
-            resources
-        );
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(location);
+            loader.setResources(resources);
+            loader.load();
+
+            Scene scene = new Scene(loader.getRoot(), WIDTH, HEIGHT);
+            stage.setScene(scene);
+
+            Closeable controller = loader.getController();
+            stage.setOnCloseRequest(_ -> this.closeController(controller));
+
+            LOGGER.info("Loaded main screen '{}'.", location);
+        } catch (Exception e) {
+            new StackTraceDialog()
+                .setTitle("HashTools")
+                .setThrowable(e)
+                .show();
+            LOGGER.error("Failed to load the main screen.", e);
+        }
 
 
 
-        Scene scene = loadingResult.scene();
-        Closeable controller = loadingResult.controller();
-
-
-
-        stage.setScene(scene);
         stage.setTitle("HashTools");
         stage.show();
-        stage.setOnCloseRequest(_ -> this.closeController(controller));
     }
 
 
-
-    private LoadingResult loadFxml(URL location, ResourceBundle resources) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(location);
-        loader.setResources(resources);
-
-        Pane pane = loader.load();
-        LOGGER.debug("Loaded screen '{}'.", location);
-
-        Scene scene = new Scene(pane, 853.0, 480.0);
-        MainController controller = loader.getController();
-
-        return new LoadingResult(scene, controller);
-    }
 
     private void closeController(Closeable controller) {
         try {
             controller.close();
-            LOGGER.debug("Closed the controller: '{}'.", controller);
-        } catch (IOException e) {
+            LOGGER.debug("Closed the controller '{}'.", controller);
+        } catch (Exception e) {
+            LOGGER.error("Failed to close the controller '{}'.", controller);
             throw new RuntimeException(e);
         }
     }
-
-
-
-    private record LoadingResult(
-        Scene scene,
-        Closeable controller
-    ) {}
 }
