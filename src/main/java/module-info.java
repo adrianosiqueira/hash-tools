3c10b2edc3a9893964dd0b9b084f38b2211c7a23
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-06-01 10:53:07 -0300

    Remove the 'ChecksumIdentifier' strategy.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,6 @@ module hash.tools {
     exports hashtools.core.source.checksum;
     exports hashtools.core.source.input;
     exports hashtools.core.strategy.checksumextractor;
-    exports hashtools.core.strategy.checksumidentifier;
     exports hashtools.core.strategy.formatter;
     exports hashtools.core.threadpool;
     exports hashtools.module.checking.controller;
@@ -25,7 +24,6 @@ module hash.tools {
     opens hashtools.core.source.checksum;
     opens hashtools.core.source.input;
     opens hashtools.core.strategy.checksumextractor;
-    opens hashtools.core.strategy.checksumidentifier;
     opens hashtools.core.strategy.formatter;
     opens hashtools.core.threadpool;
     opens hashtools.module.checking.controller;
