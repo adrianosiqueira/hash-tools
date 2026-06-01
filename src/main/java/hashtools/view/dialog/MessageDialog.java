ff4321f2ee403f7ba8906ed83f00adf3f478fbba
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-05-31 23:37:36 -0300

    Refactor the 'MessageDialog' to allow several kind of contents.

--- src/main/java/hashtools/view/dialog/MessageDialog.java
+++ src/main/java/hashtools/view/dialog/MessageDialog.java
@@ -1,15 +1,22 @@
 package hashtools.view.dialog;
 
-import hashtools.core.model.ThrowableWrapper;
+import hashtools.core.model.Problem;
 import javafx.application.Platform;
-import javafx.geometry.Dimension2D;
-import javafx.geometry.Rectangle2D;
+import javafx.collections.ObservableMap;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Screen;
 
-import java.util.Optional;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
@@ -18,62 +25,128 @@ public class MessageDialog {
 
     private String title;
     private String header;
-    private String content;
+    private Node content;
 
     private int autoCloseTime;
     private TimeUnit autoCloseTimeUnit;
+    private Thread autoCloseThread;
 
 
 
     public MessageDialog() {
-        this.withTitle(null);
-        this.withHeader(null);
-        this.setContent(null);
-        this.setAutoClose(0, null);
+        this.title = "";
+        this.header = "";
+        this.content = null;
+
+        this.autoCloseTime = Integer.MAX_VALUE;
+        this.autoCloseTimeUnit = TimeUnit.DAYS;
     }
 
 
 
     public MessageDialog withTitle(String title) {
-        this.title = Optional
-            .ofNullable(title)
-            .orElse("");
-
+        this.title = Objects.requireNonNullElse(title, "");
         return this;
     }
 
     public MessageDialog withHeader(String header) {
-        this.header = Optional
-            .ofNullable(header)
-            .orElse("");
-
+        this.header = Objects.requireNonNullElse(header, "");
         return this;
     }
 
     public MessageDialog withContent(String message) {
-        Optional
-            .ofNullable(message)
-            .filter(m -> !m.isBlank())
-            .ifPresent(this::setContent);
+        if (message == null) {
+            return this;
+        }
 
-        return this;
-    }
 
-    public MessageDialog withContent(ThrowableWrapper throwable) {
-        Optional
-            .ofNullable(throwable)
-            .map(ThrowableWrapper::getStackTrace)
-            .ifPresent(this::setContent);
+
+        String fxml = "/hashtools/fxml/message-dialog-message-screen.fxml";
+        URL location = this.getClass().getResource(fxml);
+
+        if (location == null) {
+            throw new RuntimeException("FXML not found: " + fxml);
+        }
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(location);
+            content = loader.load();
+
+            ObservableMap<String, Object> namespace = loader.getNamespace();
+            ((Label) namespace.get("lblMessage")).setText(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         return this;
     }
 
     public MessageDialog withContent(Throwable throwable) {
-        Optional
-            .ofNullable(throwable)
-            .map(ThrowableWrapper::new)
-            .map(ThrowableWrapper::getStackTrace)
-            .ifPresent(this::setContent);
+        if (throwable == null) {
+            return this;
+        }
+
+
+
+        StringWriter content = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(content));
+
+
+
+        String fxml = "/hashtools/fxml/message-dialog-throwable-screen.fxml";
+        URL location = this.getClass().getResource(fxml);
+
+        if (location == null) {
+            throw new RuntimeException("FXML not found: " + fxml);
+        }
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(location);
+            this.content = loader.load();
+
+            ObservableMap<String, Object> namespace = loader.getNamespace();
+            ((TextArea) namespace.get("txtContent")).setText(content.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return this;
+    }
+
+    public MessageDialog withContent(Problem problem) {
+        if (problem == null) {
+            return this;
+        }
+
+
+
+        String fxml = "/hashtools/fxml/message-dialog-problem-screen.fxml";
+        URL location = this.getClass().getResource(fxml);
+
+        if (location == null) {
+            throw new RuntimeException("FXML not found: " + fxml);
+        }
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(location);
+            content = loader.load();
+
+            ObservableMap<String, Object> namespace = loader.getNamespace();
+            ((Label) namespace.get("lblDescription")).setText(problem.getDescription());
+            ((Label) namespace.get("lblCause")).setText(problem.getCause());
+            ((Label) namespace.get("lblFix")).setText(problem.getFix());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         return this;
     }
@@ -85,26 +158,10 @@ public class MessageDialog {
 
 
 
-        this.setAutoClose(autoCloseTime, autoCloseTimeUnit);
+        this.autoCloseTime = Math.abs(autoCloseTime);
+        this.autoCloseTimeUnit = autoCloseTimeUnit;
+
         return this;
-    }
-
-
-
-    private void setContent(String content) {
-        this.content = Optional
-            .ofNullable(content)
-            .orElse("");
-    }
-
-    private void setAutoClose(int autoCloseTime, TimeUnit autoCloseTimeUnit) {
-        this.autoCloseTime = autoCloseTime > 0
-            ? autoCloseTime
-            : Integer.MAX_VALUE;
-
-        this.autoCloseTimeUnit = Optional
-            .ofNullable(autoCloseTimeUnit)
-            .orElse(TimeUnit.DAYS);
     }
 
 
@@ -147,62 +204,30 @@ public class MessageDialog {
         dialog.setResizable(true);
         dialog.setTitle(title);
         dialog.setHeaderText(header);
+        dialog.setOnCloseRequest(_ -> {
+            if (autoCloseThread != null) {
+                autoCloseThread.interrupt();
+            }
+        });
 
 
 
-        String style = """
-            -fx-background-color: white;
-            -fx-border-color: transparent;
-            -fx-border-width: 0;
-            """;
-
-        TextArea txtContent = new TextArea();
-        txtContent.setEditable(false);
-        txtContent.setText(content);
-        txtContent.setStyle(style);
-
-        dialog
-            .getDialogPane()
-            .setContent(txtContent);
-
-
-
-        dialog
-            .getDialogPane()
+        DialogPane dialogPane = dialog.getDialogPane();
+        dialogPane.setContent(content);
+        dialogPane
             .getButtonTypes()
             .setAll(ButtonType.OK);
-
-
-
-        Dimension2D dimension = this.calculateDimension();
-        dialog.setWidth(dimension.getWidth());
-        dialog.setHeight(dimension.getHeight());
+        dialogPane.setMaxWidth(Screen
+            .getPrimary()
+            .getBounds()
+            .getWidth()
+            * 0.25
+        );
 
 
 
         showCommand.accept(dialog);
         this.scheduleDialogClosing(dialog);
-    }
-
-    private Dimension2D calculateDimension() {
-        Rectangle2D screen = Screen
-            .getPrimary()
-            .getBounds();
-
-        double width = screen
-            .getWidth()
-            * 0.25;
-
-        double height = screen
-            .getHeight()
-            * 0.25;
-
-
-
-        return new Dimension2D(
-            width,
-            height
-        );
     }
 
     private void scheduleDialogClosing(Dialog<?> dialog) {
@@ -213,7 +238,7 @@ public class MessageDialog {
             } catch (Exception ignored) {}
         };
 
-        Thread
+        autoCloseThread = Thread
             .ofPlatform()
             .daemon()
             .start(closeDialog);
