6849580e4c4d3187784babbafddb284005c01c96
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-06-07 14:29:32 -0300

    Append a new line in the formatted result.

--- src/main/java/hashtools/domain/checksum/checker/model/CheckerResult.java
+++ src/main/java/hashtools/domain/checksum/checker/model/CheckerResult.java
@@ -88,7 +88,8 @@ public class CheckerResult {
             + lineSeparator
             + result
             + lineSeparator
-            + formattedReliability;
+            + formattedReliability
+            + lineSeparator;
     }
 
 
