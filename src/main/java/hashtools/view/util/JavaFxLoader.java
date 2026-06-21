4c4a35de1c72ec6d25ee45d3e978cbb1b21a447c
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-06-21 18:40:51 -0300

    Add the 'JavaFxLoader' utility.

---
+++ src/main/java/hashtools/view/util/JavaFxLoader.java
@@ -0,0 +1,34 @@
+package hashtools.view.util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+public class JavaFxLoader extends FXMLLoader {
+
+    public void setLocation(String location) {
+        URL url = this
+            .getClass()
+            .getResource(location);
+
+        super.setLocation(url);
+    }
+
+    public void setResources(String resources) {
+        ResourceBundle bundle = ResourceBundle.getBundle(
+            resources,
+            Locale.getDefault()
+        );
+
+        super.setResources(bundle);
+    }
+
+    public void consumeScene(Consumer<Scene> consumer) {
+        Scene scene = new Scene(this.getRoot());
+        consumer.accept(scene);
+    }
+}
