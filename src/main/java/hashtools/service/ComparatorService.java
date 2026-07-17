db1710624121bd71e9db2c6678ddee3bbe551acd
adrianosiqueira <as.adriano.siqueira@gmail.com>
2026-07-17 11:12:21 -0300

    Generate the checksums in parallel.

--- src/main/java/hashtools/service/ComparatorService.java
+++ src/main/java/hashtools/service/ComparatorService.java
@@ -2,17 +2,31 @@ package hashtools.service;
 
 import hashtools.domain.algorithm.Algorithm;
 import hashtools.domain.algorithm.MessageDigestProxy;
+import hashtools.domain.checksum.Checksum;
 import hashtools.domain.checksum.ComparatorChecksum;
 import hashtools.domain.container.ChecksumComparisonContainer;
 import hashtools.domain.context.ChecksumComparisonContext;
 import hashtools.domain.result.ChecksumComparisonResult;
+import hashtools.strategy.thread.VirtualThreadFactory;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadFactory;
 
 public class ComparatorService {
 
+    private ChecksumComparisonContext context;
+
+
+
     public ChecksumComparisonContainer performChecksumComparison(ChecksumComparisonContext context) {
+        this.context = context;
+
+
+
         // Problem detection
         String problem = context
             .detectProblem()
@@ -25,30 +39,91 @@ public class ComparatorService {
 
 
         // Processing data
-        Algorithm algorithm = context.getAlgorithm();
-        MessageDigestProxy messageDigestProxy1 = algorithm.createMessageDigestProxy();
-        MessageDigestProxy messageDigestProxy2 = algorithm.createMessageDigestProxy();
+        CompletableFuture<Checksum> checksum1 = new CompletableFuture<>();
+        CompletableFuture<Checksum> checksum2 = new CompletableFuture<>();
+        ThreadFactory threadFactory = new VirtualThreadFactory();
+
+
+
+        // Processing
+        threadFactory
+            .newThread(() -> this.generateChecksum1(checksum1))
+            .start();
+
+        threadFactory
+            .newThread(() -> this.generateChecksum2(checksum2))
+            .start();
 
 
 
         try {
-            // Processing
-            context.updateMessageDigests1(List.of(messageDigestProxy1.messageDigest()));
-            context.updateMessageDigests2(List.of(messageDigestProxy2.messageDigest()));
-
-
-
             // Result collecting
             ComparatorChecksum checksum = new ComparatorChecksum();
-            checksum.setChecksum1(messageDigestProxy1.decodeIntoChecksum());
-            checksum.setChecksum2(messageDigestProxy2.decodeIntoChecksum());
+            checksum.setChecksum1(checksum1.get());
+            checksum.setChecksum2(checksum2.get());
 
             ChecksumComparisonResult result = new ChecksumComparisonResult();
             result.setChecksum(checksum);
 
             return ChecksumComparisonContainer.result(result);
-        } catch (IOException e) {
+        } catch (Exception e) {
             return ChecksumComparisonContainer.exception(e);
+        }
+    }
+
+
+
+    private void generateChecksum1(CompletableFuture<Checksum> checksum1) {
+        Collection<MessageDigestProxy> messageDigestProxies = context
+            .getAlgorithms()
+            .stream()
+            .map(Algorithm::createMessageDigestProxy)
+            .toList();
+
+        List<MessageDigest> messageDigests = messageDigestProxies
+            .stream()
+            .map(MessageDigestProxy::messageDigest)
+            .toList();
+
+        try {
+            context.updateMessageDigests1(messageDigests);
+
+            Checksum checksum = messageDigestProxies
+                .stream()
+                .map(MessageDigestProxy::decodeIntoChecksum)
+                .toList()
+                .getFirst();
+
+            checksum1.complete(checksum);
+        } catch (IOException e) {
+            checksum1.completeExceptionally(e);
+        }
+    }
+
+    private void generateChecksum2(CompletableFuture<Checksum> checksum2) {
+        Collection<MessageDigestProxy> messageDigestProxies = context
+            .getAlgorithms()
+            .stream()
+            .map(Algorithm::createMessageDigestProxy)
+            .toList();
+
+        List<MessageDigest> messageDigests = messageDigestProxies
+            .stream()
+            .map(MessageDigestProxy::messageDigest)
+            .toList();
+
+        try {
+            context.updateMessageDigests2(messageDigests);
+
+            Checksum checksum = messageDigestProxies
+                .stream()
+                .map(MessageDigestProxy::decodeIntoChecksum)
+                .toList()
+                .getFirst();
+
+            checksum2.complete(checksum);
+        } catch (IOException e) {
+            checksum2.completeExceptionally(e);
         }
     }
 }
